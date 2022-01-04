967a1e81c52ce4b92e0bf72dccd2a34de1d48f50
thjang94 <thjang94@naver.com>
2022-01-04 18:20:00 +0900

    update : 구조 변경

---
+++ keep_distance_back/src/main/java/keep_distance_back/com/KeepDistanceBackApplication.java
@@ -0,0 +1,15 @@
+package keep_distance_back.com;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepDistanceBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepDistanceBackApplication.class, args);
+	}
+
+}
+ 
---
+++ keep_distance_back/src/test/java/keep_distance_back/com/KeepDistanceBackApplicationTests.java
@@ -0,0 +1,14 @@
+package keep_distance_back.com;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeepDistanceBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
